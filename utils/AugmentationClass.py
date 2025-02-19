d5f87dfede2d38db274a5857791d84355d18bb8f
Aaron Luo <74880925+AaronLuo-ai@users.noreply.github.com>
2025-02-18 20:37:43 -0600

    Update AugmentationClass.py

--- utils/AugmentationClass.py
+++ utils/AugmentationClass.py
@@ -4,12 +4,18 @@ from torchvision.transforms import InterpolationMode
 from torchvision import transforms
 from torchvision.transforms.functional import InterpolationMode, adjust_brightness
 
+
 class JointTransformTrain:
-    def __init__(self, degrees, transform_image, transform_mask, interpolation=InterpolationMode.NEAREST):
+    def __init__(
+        self,
+        degrees,
+        transform_image,
+        transform_mask,
+        interpolation=InterpolationMode.NEAREST,
+    ):
         self.degrees = degrees
         self.interpolation = interpolation
         # Define the other transforms
-        self.resize = transforms.Resize((128, 128))
         self.transform_image = transform_image
         self.transform_mask = transform_mask
         self.flip_prob = 0.5
@@ -17,19 +23,26 @@ class JointTransformTrain:
     def __call__(self, image, mask):
         # Generate a single random rotation angle for both image and mask
         angle = random.uniform(-self.degrees, self.degrees)
+
         transformed_image = self.transform_image(image)
         transformed_mask = self.transform_mask(mask)
-        rotated_image = F.rotate(transformed_image, angle, interpolation=self.interpolation)
-        rotated_mask = F.rotate(transformed_mask, angle, interpolation=self.interpolation)
+
+        rotated_image = F.rotate(
+            transformed_image, angle, interpolation=self.interpolation
+        )
+        rotated_mask = F.rotate(
+            transformed_mask, angle, interpolation=self.interpolation
+        )
 
         if random.random() < self.flip_prob:
             rotated_image = F.hflip(rotated_image)
             rotated_mask = F.hflip(rotated_mask)
 
-        brightness_factor = random.randint(1, 10)
-        brightened_image = adjust_brightness(rotated_image, brightness_factor=brightness_factor)
-        brightened_mask = adjust_brightness(rotated_mask, brightness_factor=brightness_factor)
-
+        brightness_factor = random.random() / 2.5 + 0.8  # scales between [0.8, 1.2)
+        brightened_image = adjust_brightness(
+            rotated_image, brightness_factor=brightness_factor
+        )
+        brightened_mask = rotated_mask
         return brightened_image, brightened_mask
 
 
